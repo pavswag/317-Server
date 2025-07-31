57d5b542ccef6e757dfe763071c26cbe46c910d2
Pav <53246348+pavswag@users.noreply.github.com>
2025-07-31 16:25:56 -0700

    Add respawning mobs for boss instances

--- src/io/xeros/content/instances/BossInstanceManager.java
+++ src/io/xeros/content/instances/BossInstanceManager.java
@@ -3,6 +3,7 @@ package io.xeros.content.instances;
 import io.xeros.content.instances.InstanceConfiguration;
 import io.xeros.content.instances.impl.LegacySoloPlayerInstance;
 import io.xeros.model.Npcs;
+import io.xeros.model.definitions.NpcStats;
 import io.xeros.model.entity.npc.NPC;
 import io.xeros.model.entity.npc.NPCSpawning;
 import io.xeros.model.entity.player.Boundary;
@@ -27,15 +28,53 @@ public class BossInstanceManager {
 
         /** Player that owns this instance. */
         private final Player owner;
+        /** Tier of this instance used for respawn times. */
+        private final BossTier tier;
 
-        BossInstanceArea(Player owner, Boundary boundary) {
+        BossInstanceArea(Player owner, BossTier tier, Boundary boundary) {
             super(InstanceConfiguration.CLOSE_ON_EMPTY, owner, boundary);
             this.owner = owner;
+            this.tier = tier;
         }
 
         @Override
         public void onDispose() {
             INSTANCES.remove(owner);
+        }
+
+        public BossTier getTier() {
+            return tier;
+        }
+    }
+
+    /** Description for each NPC spawned in a tier. */
+    public static class BossMob {
+        private final int npcId;
+        private final int hitpoints;
+        private final int attack;
+        private final int defence;
+
+        public BossMob(int npcId, int hitpoints, int attack, int defence) {
+            this.npcId = npcId;
+            this.hitpoints = hitpoints;
+            this.attack = attack;
+            this.defence = defence;
+        }
+
+        public int getNpcId() {
+            return npcId;
+        }
+
+        public int getHitpoints() {
+            return hitpoints;
+        }
+
+        public int getAttack() {
+            return attack;
+        }
+
+        public int getDefence() {
+            return defence;
         }
     }
 
@@ -43,9 +82,12 @@ public class BossInstanceManager {
      * Difficulty tiers for bosses.
      */
     public enum BossTier {
-        TIER1("Training Grounds", 0, 0, -1, new int[]{Npcs.COW}),
-        TIER2("Giants' Den", 10, 100_000, -1, new int[]{Npcs.HILL_GIANT}),
-        TIER3("Dragon Lair", 50, 1_000_000, 11286, new int[]{Npcs.KING_BLACK_DRAGON});
+        TIER1("Training Grounds", 0, 0, -1, 5,
+                new BossMob[]{new BossMob(Npcs.COW, 10, 1, 1)}),
+        TIER2("Giants' Den", 10, 100_000, -1, 20,
+                new BossMob[]{new BossMob(Npcs.HILL_GIANT, 35, 20, 20)}),
+        TIER3("Dragon Lair", 50, 1_000_000, 11286, 40,
+                new BossMob[]{new BossMob(Npcs.KING_BLACK_DRAGON, 250, 150, 150)});
 
         private final String zoneName;
         /** Kill requirement to unlock this tier. */
@@ -54,14 +96,16 @@ public class BossInstanceManager {
         private final int gpCost;
         /** Optional item requirement (-1 if none). */
         private final int itemRequirement;
-        private final int[] npcIds;
+        private final int respawnTime;
+        private final BossMob[] mobs;
 
-        BossTier(String zoneName, int killRequirement, int gpCost, int itemRequirement, int[] npcIds) {
+        BossTier(String zoneName, int killRequirement, int gpCost, int itemRequirement, int respawnTime, BossMob[] mobs) {
             this.zoneName = zoneName;
             this.killRequirement = killRequirement;
             this.gpCost = gpCost;
             this.itemRequirement = itemRequirement;
-            this.npcIds = npcIds;
+            this.respawnTime = respawnTime;
+            this.mobs = mobs;
         }
 
         public String getZoneName() {
@@ -80,8 +124,12 @@ public class BossInstanceManager {
             return itemRequirement;
         }
 
-        public int[] getNpcIds() {
-            return npcIds;
+        public int getRespawnTime() {
+            return respawnTime;
+        }
+
+        public BossMob[] getMobs() {
+            return mobs;
         }
     }
 
@@ -104,7 +152,7 @@ public class BossInstanceManager {
         Boundary bounds = new Boundary(player.getX() - 10, player.getY() - 10,
                 player.getX() + 10, player.getY() + 10);
 
-        BossInstanceArea instance = new BossInstanceArea(player, bounds);
+        BossInstanceArea instance = new BossInstanceArea(player, tier, bounds);
         INSTANCES.put(player, instance);
 
         instance.add(player);
@@ -121,18 +169,24 @@ public class BossInstanceManager {
         int baseX = player.getX();
         int baseY = player.getY();
 
-        int[] ids = tier.getNpcIds();
-        for (int index = 0; index < ids.length; index++) {
-            int npcId = ids[index];
+        BossMob[] mobs = tier.getMobs();
+        for (int index = 0; index < mobs.length; index++) {
+            BossMob mob = mobs[index];
 
             // Spread NPCs out using a 3xN grid with 2 tile spacing
             int offsetX = (index % 3) * 2;
             int offsetY = (index / 3) * 2;
 
-            NPC npc = NPCSpawning.spawnNpc(player, npcId, baseX + offsetX, baseY + offsetY,
-                    instance.getHeight(), 0, 0, false, false);
+            NPC npc = NPCSpawning.spawnNpc(player, mob.getNpcId(), baseX + offsetX, baseY + offsetY,
+                    instance.getHeight(), 0, 0, false, false,
+                    NpcStats.builder()
+                            .setHitpoints(mob.getHitpoints())
+                            .setAttackLevel(mob.getAttack())
+                            .setDefenceLevel(mob.getDefence())
+                            .createNpcStats());
             if (npc != null) {
-                npc.getBehaviour().setRespawn(false);
+                npc.getBehaviour().setRespawn(true);
+                npc.getBehaviour().setRespawnWhenPlayerOwned(true);
                 instance.add(npc);
             }
         }
