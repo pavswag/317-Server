6de9197071a6e39f6a4401dd18cb34550cd133e2
Pav <53246348+pavswag@users.noreply.github.com>
2025-08-06 09:57:41 -0700

    fix: limit clue reward display

--- src/io/xeros/content/trails/TreasureTrails.java
+++ src/io/xeros/content/trails/TreasureTrails.java
@@ -52,7 +52,7 @@ public class TreasureTrails {
 		return 0;
 	}
 
-	private static void announceRare(Player player, GameItem item, RewardLevel difficulty) {
+        public static void announceRare(Player player, GameItem item, RewardLevel difficulty) {
 		PlayerHandler.executeGlobalMessage("[<col=CC0000>Treasure</col>] <col=255>" + player.getDisplayNameFormatted() + "</col> " +
 				"<col=CC0000>received " +
 				"<col=255>" + ItemDef.forId(item.getId()).getName() + "</col> " +
@@ -69,24 +69,29 @@ public class TreasureTrails {
 	 * This method is used by clues.
 	 * Rolls=1+rand(1, 2)
 	 */
-	public List<GameItem> generateRewardList(RewardLevel rewardLevel) {
-		return generateRewardList(rewardLevel, 1 + Misc.random(2));
-	}
+        public List<GameItem> generateRewardList(RewardLevel rewardLevel) {
+                return generateRewardList(rewardLevel, 1 + Misc.random(2), true);
+        }
 
-	/**
-	 * This will generate the drop list, globally announce rares and update collection log. It's assumed
-	 * that you will be dropped/adding these items immediately after calling!
-	 * @param rewardLevel The {@link RewardLevel}
-	 * @return List of random items.
-	 */
-	public List<GameItem> generateRewardList(RewardLevel rewardLevel, int rolls) {
-		player.getNpcDeathTracker().add(Misc.optimizeText(rewardLevel.name().toLowerCase()), -1, 0);
-		List<GameItem> rewards = TreasureTrailsRewards.getRandomRewardItems(rewardLevel, rolls);
+        public List<GameItem> generateRewardList(RewardLevel rewardLevel, int rolls) {
+                return generateRewardList(rewardLevel, rolls, true);
+        }
 
-		for (GameItem item : rewards) {
-			if (ItemDef.forId(item.getId()).getName().contains("3rd") || item.getId() == 2577 || ItemDef.forId(item.getId()).getName().contains("mage's")) {
-				announceRare(player, item, rewardLevel);
-			}
+        /**
+         * This will generate the drop list, optionally globally announce rares and update collection log. It's assumed
+         * that you will be dropped/adding these items immediately after calling!
+         * @param rewardLevel The {@link RewardLevel}
+         * @param announce should global announcements be sent for rare items
+         * @return List of random items.
+         */
+        public List<GameItem> generateRewardList(RewardLevel rewardLevel, int rolls, boolean announce) {
+                player.getNpcDeathTracker().add(Misc.optimizeText(rewardLevel.name().toLowerCase()), -1, 0);
+                List<GameItem> rewards = TreasureTrailsRewards.getRandomRewardItems(rewardLevel, rolls);
+
+                for (GameItem item : rewards) {
+                        if (announce && (ItemDef.forId(item.getId()).getName().contains("3rd") || item.getId() == 2577 || ItemDef.forId(item.getId()).getName().contains("mage's"))) {
+                                announceRare(player, item, rewardLevel);
+                        }
 
 			if (TreasureTrailsRewards.possibleDrops.get(rewardLevel).stream().anyMatch(it -> it.getItemId() == item.getId())) {
 				player.getCollectionLog().handleDrop(player, rewardLevel.ordinal(), item.getId(), item.getAmount());
@@ -118,27 +123,29 @@ public class TreasureTrails {
 		displayRewards(rewards);
 	}
 
-	public void displayRewards(List<GameItem> rewards) {
-		player.outStream.createFrameVarSizeWord(53);
-		player.outStream.writeUnsignedWord(6963);
-		player.outStream.writeUnsignedWord(rewards.size());
-		for (int i = 0; i < rewards.size(); i++) {
-			if (player.playerItemsN[i] > 254) {
-				player.outStream.writeByte(255);
-				player.outStream.writeDWord_v2(rewards.get(i).getAmount());
-			} else {
-				player.outStream.writeByte(rewards.get(i).getAmount());
-			}
-			if (rewards.size() > 0) {
-				player.outStream.writeWordBigEndianA(rewards.get(i).getId() + 1);
-			} else {
-				player.outStream.writeWordBigEndianA(0);
-			}
-		}
-		player.outStream.endFrameVarSizeWord();
-		player.flushOutStream();
-		player.getPA().showInterface(6960);
-	}
+        public void displayRewards(List<GameItem> rewards) {
+                int displayCount = Math.min(28, rewards.size());
+                player.outStream.createFrameVarSizeWord(53);
+                player.outStream.writeUnsignedWord(6963);
+                player.outStream.writeUnsignedWord(displayCount);
+                for (int i = 0; i < displayCount; i++) {
+                        GameItem reward = rewards.get(i);
+                        if (reward.getAmount() > 254) {
+                                player.outStream.writeByte(255);
+                                player.outStream.writeDWord_v2(reward.getAmount());
+                        } else {
+                                player.outStream.writeByte(reward.getAmount());
+                        }
+                        if (reward.getId() > 0) {
+                                player.outStream.writeWordBigEndianA(reward.getId() + 1);
+                        } else {
+                                player.outStream.writeWordBigEndianA(0);
+                        }
+                }
+                player.outStream.endFrameVarSizeWord();
+                player.flushOutStream();
+                player.getPA().showInterface(6960);
+        }
 
 	private static void openCasket(Player player, RewardLevel rewardLevel) {
 		if (player.getItems().freeSlots() < 3) {
