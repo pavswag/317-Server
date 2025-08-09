03b13bbfd9d4fc6b46acc2e22dfbd7c8af0c8379
Pav <53246348+pavswag@users.noreply.github.com>
2025-08-09 16:31:25 -0700

    Add Demon Hunter perks and damage scaling

--- src/io/xeros/content/skills/slayer/DemonHunterTaskManager.java
+++ src/io/xeros/content/skills/slayer/DemonHunterTaskManager.java
@@ -4,6 +4,9 @@ import io.xeros.content.skills.Skill;
 import io.xeros.model.entity.npc.NPC;
 import io.xeros.model.entity.player.Player;
 
+import static io.xeros.content.skills.slayer.DemonHunterPerks.Perk;
+import io.xeros.content.skills.slayer.DemonHunterPerks;
+
 import java.util.Optional;
 
 /**
@@ -21,7 +24,8 @@ public class DemonHunterTaskManager {
         int baseXp = boss.getBaseXp();
 
         if (player.getDemonHunterTask().isPresent() && player.getDemonHunterTask().get().getBoss() == boss) {
-            int remaining = player.getDemonHunterTaskProgress() - 1;
+            int decrement = DemonHunterPerks.has(player, Perk.FAST_TRACK) ? 2 : 1;
+            int remaining = player.getDemonHunterTaskProgress() - decrement;
             player.setDemonHunterTaskProgress(Math.max(0, remaining));
 
             int xp = DemonHunterXPTable.getXPFor(baseXp, boss.getTier());
@@ -39,6 +43,9 @@ public class DemonHunterTaskManager {
             player.getPA().addSkillXPMultiplied(baseXp, Skill.SLAYER.getId(), true);
             DemonSlayerLeaderboardManager.addXp(player, xp);
             player.addDemonMarks(1);
+            if (DemonHunterPerks.has(player, Perk.MARK_MASTER)) {
+                player.addDemonMarks(1);
+            }
 
             if (remaining <= 0) {
                 player.sendMessage("\uD83C\uDFAF Task Complete: You've slain " + boss.getNpcName() + "! +" + (xp * player.getDemonHunterTask().get().getAmount()) + " Demon Hunter XP");
